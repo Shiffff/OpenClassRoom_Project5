7693932275134a65d47a1bba12e0d7f0b2868ca7
shiffff <alexis.privat@live.fr>
2022-06-15 23:18:06 +0200

    Send vers localstorage OK

--- front/js/product.js
+++ front/js/product.js
@@ -3,7 +3,6 @@ let resultID = params.get('id')
 let urlApiProduct = `http://localhost:3000/api/products/${resultID}`;
 
 
-
 //********************************************************************* */
 fetch(urlApiProduct).then((response) =>
 response.json().then((data) => {
@@ -13,15 +12,38 @@ response.json().then((data) => {
     document.querySelector("article div.item__img") .innerHTML = `<img src="${data.imageUrl}" alt=${data.altTxt}>`;
     couleurs = data.colors
     let printcolor = [];
-    console.log(couleurs)
     for  (let i = 0; i < couleurs.length; i++){
         let color = couleurs[i];
     printcolor += `<option value="${i}">${color}</option>`;
 }
 document.querySelector("#colors") .innerHTML = printcolor;
-console.log(printcolor);
+const btnCart = document.querySelector("#addToCart");
 
-})
+
+btnCart.addEventListener("click",(e)=>{
+    e.preventDefault();
+    let selectColor = document.querySelector("#colors");
+    let couleurchoisie = couleurs[selectColor.value];
+    let quantite = document.querySelector("#quantity");
+    let quantiteProduit = quantite.value;
+    let optionProduit = {
+        nomProduit: data.name,
+        idProduit: resultID,
+        couleurProduit: couleurchoisie,
+        prixProduit: data.price,
+        nombreproduit: quantiteProduit
+    }
+    let produitLocalStorage = JSON.parse(localStorage.getItem("product"));
+    if(produitLocalStorage){
+        produitLocalStorage.push(optionProduit);
+        localStorage.setItem("product", JSON.stringify(produitLocalStorage));
+    }else{
+        produitLocalStorage = [];
+        produitLocalStorage.push(optionProduit);
+        localStorage.setItem("product", JSON.stringify(produitLocalStorage));
+    }
+        })
+    })
 );
 
 
