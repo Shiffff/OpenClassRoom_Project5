08503d486639fcce910ac7f4b255b2446e797e48
shiffff <alexis.privat@live.fr>
2022-06-15 15:44:55 +0200

    Choix couleur fin

--- front/js/product.js
+++ front/js/product.js
@@ -4,15 +4,23 @@ let urlApiProduct = `http://localhost:3000/api/products/${resultID}`;
 
 
 
-
 //********************************************************************* */
 fetch(urlApiProduct).then((response) =>
 response.json().then((data) => {
-    console.log(data)
     document.querySelector("#title") .innerHTML = data.name;
     document.querySelector("#price") .innerHTML = data.price;
     document.querySelector("#description") .innerHTML = data.description;
     document.querySelector("article div.item__img") .innerHTML = `<img src="${data.imageUrl}" alt=${data.altTxt}>`;
+    couleurs = data.colors
+    let printcolor = [];
+    console.log(couleurs)
+    for  (let i = 0; i < couleurs.length; i++){
+        let color = couleurs[i];
+    printcolor += `<option value="${i}">${color}</option>`;
+}
+document.querySelector("#colors") .innerHTML = printcolor;
+console.log(printcolor);
+
 })
 );
 
@@ -20,22 +28,15 @@ response.json().then((data) => {
 
 
 
-
-
 /*
-fetch(urlApi).then((response) =>
-     response.json().then((data) => {
-        let affichage = ("");
-    for (let produit of data){
-        affichage += `<a href= ../html/product.html?id=${produit._id}>`
-        affichage += `<article>`
-        affichage += `<img id="img" src="${produit.imageUrl}" alt=${produit.altTxt}>`;
-        affichage += `<h3 class="productName">${produit.name}</h3>`;
-        affichage += `<p class="productDescription">${produit.description}</p>`;
-        affichage += `</article>`
-        affichage += `</a>`
-    }
-    document.querySelector("#items") .innerHTML = affichage;
+        let couleurs = ("");
+    for (let colors of data){
+        couleurs += `<option value="${colors.data.colors}">${colors.data.colors}</option>`;
+    } 
+    document.querySelector("#colors") .innerHTML = couleurs;
+        for (let i = 0; i < couleurs; i++) {
+        console.log("Passager embarqué !");
+     }
 })
 );
 */
